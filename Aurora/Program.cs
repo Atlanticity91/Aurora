1722e631164545ed5b3854d4ccecb6fb19222f57
Alves Quentin <atlanticity91@gmail.com>
2020-09-03 18:33:40 +0200

    Ajoutez des fichiers projet.

---
+++ Aurora/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aurora {
+
+    /// <summary>
+    /// Program sealed class
+    /// </summary>
+    /// <author>ALVES Quentin</author>
+    public sealed class Program {
+
+        /// <summary>
+        /// Main static method
+        /// </summary>
+        /// <author>ALVES Quentin</author>
+        /// <note>Program main entry point</note>
+        /// <param name="args" >Arguments pass to the program.</param>
+        public static void Main( string[] args ) {
+            Console.WriteLine( "Hello World!" );
+        }
+
+    }
+
+}
